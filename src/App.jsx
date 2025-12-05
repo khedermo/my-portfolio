2ca0c9810cd79c79f22d053ee6bf3ce4325f3c78
Khoder Mohamad <khoder98moh@gmail.com>
2025-12-05 21:44:59 +0100

    Fix Footer component file name (case sensitivity)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import SkillsAndTools from "./components/SkillsAndTools";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   const scroll = (id) => {
